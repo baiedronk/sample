ba4da52f1dde9dd0a853a8bc039b8ea610204b31
baiedronk <baiedronk@yahoo.com>
2023-09-20 15:57:44 -0400

    Add files via upload

---
+++ sample/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace sample
+{
+  public class Program
+  {
+    private enum SomeOption
+    {
+      None = 0,
+      Step1,
+      Step2,
+      FinalStep
+    }
+
+    private static void Main(string[] args)
+    {
+      var usedVar = "";
+      var unusedVar = ""; // raise unused vars
+
+      Console.WriteLine("Hello, World!");
+      var some = new SomeClass() { Id = 1, Name = "sample" };
+
+      // throw too many parameters in a function
+      DoSomething(1, true, new string[1], SomeOption.None, DateTime.Now, 75, some, usedVar);
+    }
+
+    private static void DoSomething(int v1, bool v2, string[] strings, SomeOption none, DateTime now, int v3, SomeClass someClass, string someVar)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ sample/SomeClass.cs
@@ -0,0 +1,25 @@
+﻿namespace sample
+{
+  internal class SomeClass
+  {
+    // No Params
+    public SomeClass()
+    {
+    }
+
+    /// <summary>
+    /// Some Class CTOR
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="name"></param>
+    public SomeClass(int id, string name)
+    {
+      // Throw REMOVE comments
+      this.Id = id;
+      this.Name = name;
+    }
+
+    public int Id { get; set; }
+    public string? Name { get; set; }
+  }
+}
