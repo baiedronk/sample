f32b772153f75147de69074a5c38f2dc3fd7aa0d
Baie Dronk <baiedronk@yahoo.com>
2023-09-20 16:25:50 -0400

    tests added
    passing tests added

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+namespace tests
+{
+  public class Tests
+  {
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void PassSomething()
+    {
+      Assert.Pass();
+    }
+
+    [Test]
+    public void PassSomethingElse()
+    {
+      Assert.Pass();
+    }
+  }
+}
